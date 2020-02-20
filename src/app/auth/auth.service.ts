39d279c51024c03739588e92d3cbd4b54b326534
Andrey Melnikov <vafilor@gmail.com>
2020-02-20 15:07:43 -0800

    Fixed issue where cookie wasn't being set. Also made cookie required.

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -7,8 +7,15 @@ export class AuthService {
 
   get isLoggedIn(): boolean {
     const authToken = localStorage.getItem('auth-token');
+    let cookieTokenExists = false;
 
-    return !!authToken;
+    for(const cookie of document.cookie.split(';')) {
+      if(cookie.indexOf('auth-token') >= 0) {
+        cookieTokenExists = true;
+      }
+    }
+
+    return !!authToken && cookieTokenExists;
   }
 
   redirectUrl?: string;
@@ -17,13 +24,13 @@ export class AuthService {
 
   setAuthToken(token: string) {
     localStorage.setItem('auth-token', token);
-    const expires: Date = new Date(new Date().setFullYear(new Date().getFullYear() + 10));
-    document.cookie = 'auth-token=' + token + ';path=/;expires=' + expires + ';secure;';
+    const expires = new Date(new Date().setFullYear(new Date().getFullYear() + 10)).toUTCString();
+    document.cookie = 'auth-token=' + token + ';path=/;expires=' + expires;
   }
 
   clearTokens() {
     localStorage.removeItem('auth-token');
-    document.cookie = 'auth-token=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;secure;';
+    document.cookie = 'auth-token=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT;';
   }
 
   takeRedirectUrl(defaultValue = '/'): string|undefined {
