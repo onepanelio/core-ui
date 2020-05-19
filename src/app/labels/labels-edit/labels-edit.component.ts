b254b91ea2b401b0ab03de6ead144c010736825a
Andrey Melnikov <vafilor@gmail.com>
2020-05-18 20:14:20 -0700

    feature: added duplicate check for labels in labels edit component

--- src/app/labels/labels-edit/labels-edit.component.ts
+++ src/app/labels/labels-edit/labels-edit.component.ts
@@ -101,7 +101,34 @@ export class LabelsEditComponent implements OnInit {
       }
     }
 
+    let duplicate = false;
+    for(const duplicateKey of this.findDuplicateKeys()) {
+      duplicateKey.controlKey.setErrors({'duplicate': true});
+      duplicate = true;
+    }
+
+    if(duplicate) {
+      return false;
+    }
+
     return true;
+  }
+
+  private findDuplicateKeys(): LabelItem[] {
+    let keyMap = new Map<string, LabelItem>();
+
+    let duplicates = [];
+
+    for(const item of this.items) {
+      const key = item.controlKey.value;
+      if(keyMap.has(key)) {
+        duplicates.push(item);
+      } else {
+        keyMap.set(key, item);
+      }
+    }
+
+    return duplicates;
   }
 
   markAllAsDirty() {
