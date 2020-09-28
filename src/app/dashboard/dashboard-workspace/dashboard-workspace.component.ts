e252216164cc6a0c12badb7587c761e9bf98fccc
Andrey Melnikov <vafilor@gmail.com>
2020-09-28 12:18:41 -0700

    update: added workspaces dashboard

--- src/app/dashboard/dashboard-workspace/dashboard-workspace.component.ts
+++ src/app/dashboard/dashboard-workspace/dashboard-workspace.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
+import { WorkflowExecutionPhase, WorkflowExecutionsChangedEvent } from '../../workflow/workflow-executions/workflow-executions.component';
+import { WorkspacesChangedEvent } from '../../workspace/workspaces/workspaces.component';
 
 @Component({
   selector: 'app-dashboard-workspace',
   templateUrl: './dashboard-workspace.component.html',
   styleUrls: ['./dashboard-workspace.component.scss']
 })
-export class DashboardWorkspaceComponent implements OnInit {
+export class DashboardWorkspaceComponent implements OnInit, OnDestroy {
+  // tslint:disable-next-line:variable-name
+  _namespace: string;
+  @Input() set namespace(value: string|undefined) {
+    if (value === undefined) {
+      return;
+    }
+
+    this._namespace = value;
+    // TODO
+    // this.getWorkflowExecutionStatistics();
+  }
+  get namespace(): string {
+    return this._namespace;
+  }
+
+  hasWorkspaces?: boolean;
 
   constructor() { }
 
   ngOnInit() {
+    // this.getWorkflowExecutionStatistics();
+    // this.workflowsStatisticsInterval = setInterval(() => {
+    //   this.getWorkflowExecutionStatistics();
+    // }, 5000);
   }
 
+  ngOnDestroy() {
+    // if (this.workflowsStatisticsInterval) {
+    //   clearInterval(this.workflowsStatisticsInterval);
+    //   this.workflowsStatisticsInterval = undefined;
+    // }
+  }
+
+  // setPhase(phase: WorkflowExecutionPhase) {
+  //   this.workflowExecutionPhase = phase;
+  // }
+
+  clearPhase(e?: MouseEvent) {
+    if (e) {
+      e.preventDefault();
+      e.stopPropagation(); // Stop propagation so we don't trigger a setPhase event
+    }
+
+    // TODO
+    // this.workflowExecutionPhase = undefined;
+  }
+
+  onWorkspacesChanged(res: WorkspacesChangedEvent) {
+    this.hasWorkspaces = res.hasWorkspaces;
+  }
 }
