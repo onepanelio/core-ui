01fbe754ac904c0441d12096c5bb4a9f7ae3a308
Andrey Melnikov <vafilor@gmail.com>
2020-10-15 16:35:52 -0700

    chore: code updates to make codacy happy

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { AuthService } from "./auth.service";
+import { AuthService } from './auth.service';
 
 @Injectable({
   providedIn: 'root'
