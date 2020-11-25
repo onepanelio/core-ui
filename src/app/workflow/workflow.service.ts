0ac89941703293daf7f248a34b054a6eaef7f556
Andrey Melnikov <vafilor@gmail.com>
2020-11-25 10:51:30 -0800

    fix: added Omitted workflow phase. This fixes an issue where an image was missing in node status.
    Changed the logic to use a switch statement so we can make sure we catch all cases.
    
    Also ran code formatting tool on file for consistency.

--- src/app/workflow/workflow.service.ts
+++ src/app/workflow/workflow.service.ts
@@ -1,240 +1,238 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable } from 'rxjs';
 import * as yaml from 'js-yaml';
-import { WorkflowTemplateDetail } from '../workflow-template/workflow-template.service';
 import { NodeStatus } from '../node/node.service';
-import { map } from "rxjs/operators";
-import { environment } from "../../environments/environment";
-import { AuthService } from "../auth/auth.service";
-import { WorkflowExecution as ApiWorkflowExecution, WorkflowTemplate } from "../../api";
+import { environment } from '../../environments/environment';
+import { WorkflowExecution as ApiWorkflowExecution, WorkflowTemplate } from '../../api';
 
 export interface Workflow {
-  uid: string;
-  createdAt: string;
-  startedAt: string;
-  finishedAt?: string;
-  name: string;
-  manifest?: string;
-  phase?: string;
+    uid: string;
+    createdAt: string;
+    startedAt: string;
+    finishedAt?: string;
+    name: string;
+    manifest?: string;
+    phase?: string;
 }
 
-// https://github.com/argoproj/argo/issues/1849#issuecomment-565640866
-export type WorkflowPhase = 'Pending' | 'Running' | 'Succeeded' | 'Skipped' | 'Failed' | 'Error' | 'Terminated';
+// https://github.com/argoproj/argo/blob/master/pkg/apis/workflow/v1alpha1/workflow_types.go#L39
+// we add terminated for internal use
+export type WorkflowPhase = 'Pending' | 'Running' | 'Succeeded' | 'Skipped' | 'Failed' | 'Error' | 'Omitted' | 'Terminated';
 
 export interface WorkflowStatus {
-  phase: WorkflowPhase;
-  startedAt: string;
-  finishedAt: string;
-  nodes: {string: NodeStatus};
+    phase: WorkflowPhase;
+    startedAt: string;
+    finishedAt: string;
+    nodes: { string: NodeStatus };
 }
 
 export interface WorkflowManifest {
-  spec: any;
-  status: WorkflowStatus;
+    spec: any;
+    status: WorkflowStatus;
 }
 
 export class SimpleWorkflowDetail {
-  static activePhases = {
-    'Pending': true,
-    'Running': true
-  };
+    static activePhases = {
+        Pending: true,
+        Running: true
+    };
 
-  private jsonManifest: WorkflowManifest|null = null;
+    private jsonManifest: WorkflowManifest | null = null;
 
-  uid: string;
-  createdAt: string;
-  name: string;
-  manifest?: string;
-  yamlManifest?: string;
-  workflowTemplate: WorkflowTemplate;
-  _phase: WorkflowPhase;
+    uid: string;
+    createdAt: string;
+    name: string;
+    manifest?: string;
+    yamlManifest?: string;
+    workflowTemplate: WorkflowTemplate;
 
-  constructor(workflow: ApiWorkflowExecution) {
-    this.uid = workflow.uid;
-    this.createdAt = workflow.createdAt;
-    this.name = workflow.name;
-    this.manifest = workflow.manifest;
-    this.workflowTemplate = workflow.workflowTemplate;
+    // tslint:disable-next-line:variable-name
+    _phase: WorkflowPhase;
 
-    if(this.manifest) {
-      this.updateWorkflowManifest(this.manifest);
-    }
-  }
+    constructor(workflow: ApiWorkflowExecution) {
+        this.uid = workflow.uid;
+        this.createdAt = workflow.createdAt;
+        this.name = workflow.name;
+        this.manifest = workflow.manifest;
+        this.workflowTemplate = workflow.workflowTemplate;
 
-  get workflowStatus(): WorkflowStatus|null {
-    if(!this.jsonManifest) {
-      return null;
-    }
-    
-    return this.jsonManifest.status;
-  }
-
-  set phase(value: WorkflowPhase) {
-    this._phase = value;
-  }
-
-  get phase(): WorkflowPhase|null {
-    if(this._phase) {
-      return this._phase;
+        if (this.manifest) {
+            this.updateWorkflowManifest(this.manifest);
+        }
     }
 
-    if(!this.jsonManifest) {
-      return null;
+    get workflowStatus(): WorkflowStatus | null {
+        if (!this.jsonManifest) {
+            return null;
+        }
+
+        return this.jsonManifest.status;
     }
 
-    return this.jsonManifest.status.phase;
-  }
-
-  get active(): boolean {
-    const phase = this.phase;
-    if(!phase) {
-      return false;
+    set phase(value: WorkflowPhase) {
+        this._phase = value;
     }
 
-    return SimpleWorkflowDetail.activePhases[phase];
-  }
+    get phase(): WorkflowPhase | null {
+        if (this._phase) {
+            return this._phase;
+        }
 
-  get succeeded(): boolean {
-    const phase = this.phase;
-    if(!phase) {
-      return false;
+        if (!this.jsonManifest) {
+            return null;
+        }
+
+        return this.jsonManifest.status.phase;
     }
 
-    return this.phase === 'Succeeded';
-  }
+    get active(): boolean {
+        const phase = this.phase;
+        if (!phase) {
+            return false;
+        }
 
-  get terminated(): boolean {
-    const phase = this.phase;
-    if(!phase) {
-      return false;
+        return SimpleWorkflowDetail.activePhases[phase];
     }
 
-    return this.phase === 'Terminated';
-  }
+    get succeeded(): boolean {
+        const phase = this.phase;
+        if (!phase) {
+            return false;
+        }
 
-  updateWorkflowManifest(manifest: string) {
-    this.jsonManifest = JSON.parse(manifest);
-
-    let jsonTemplateManifest = yaml.safeLoad(this.workflowTemplate.manifest);
-    if (this.jsonManifest.spec.arguments && this.jsonManifest.spec.arguments.parameters) {
-      if(!jsonTemplateManifest.arguments) {
-        jsonTemplateManifest.arguments = {};
-      }
-
-      jsonTemplateManifest.arguments.parameters = this.jsonManifest.spec.arguments.parameters;
-    }
-    this.yamlManifest = yaml.safeDump(jsonTemplateManifest, {
-      lineWidth: Number.MAX_SAFE_INTEGER, // We don't want to restrict on width.
-    });
-  }
-
-  getNodeStatus(nodeId: string): NodeStatus|null {
-    const status = this.workflowStatus;
-    if(!status) {
-      return null;
+        return this.phase === 'Succeeded';
     }
 
-    return status.nodes[nodeId];
-  }
+    get terminated(): boolean {
+        const phase = this.phase;
+        if (!phase) {
+            return false;
+        }
 
-  getTemplateManifestParameters(name: string) {
-    const templates = this.jsonManifest.spec.templates;
-    for(const template of templates) {
-      if(template.name === name) {
-        // output parameters may be undefined
-        if(template.outputs.parameters) {
-          return template.outputs.parameters;
+        return this.phase === 'Terminated';
+    }
+
+    updateWorkflowManifest(manifest: string) {
+        this.jsonManifest = JSON.parse(manifest);
+
+        const jsonTemplateManifest = yaml.safeLoad(this.workflowTemplate.manifest);
+        if (this.jsonManifest.spec.arguments && this.jsonManifest.spec.arguments.parameters) {
+            if (!jsonTemplateManifest.arguments) {
+                jsonTemplateManifest.arguments = {};
+            }
+
+            jsonTemplateManifest.arguments.parameters = this.jsonManifest.spec.arguments.parameters;
+        }
+        this.yamlManifest = yaml.safeDump(jsonTemplateManifest, {
+            lineWidth: Number.MAX_SAFE_INTEGER, // We don't want to restrict on width.
+        });
+    }
+
+    getNodeStatus(nodeId: string): NodeStatus | null {
+        const status = this.workflowStatus;
+        if (!status) {
+            return null;
+        }
+
+        return status.nodes[nodeId];
+    }
+
+    getTemplateManifestParameters(name: string) {
+        const templates = this.jsonManifest.spec.templates;
+        for (const template of templates) {
+            if (template.name === name) {
+                // output parameters may be undefined
+                if (template.outputs.parameters) {
+                    return template.outputs.parameters;
+                }
+
+                return [];
+            }
         }
 
         return [];
-      }
     }
-
-    return [];
-  }
 }
 
 // todo deprecated. Remove this and use API generated WorkflowExecution.
 export class WorkflowExecution {
-  uid: string;
-  createdAt: string;
+    uid: string;
+    createdAt: string;
 
-  startedAt: string;
-  finishedAt: string|Date;
+    startedAt: string;
+    finishedAt: string | Date;
 
-  name: string;
-  phase?: string;
+    name: string;
+    phase?: string;
 
-  private jsonManifest: WorkflowManifest|null = null;
+    private jsonManifest: WorkflowManifest | null = null;
 
-  constructor(workflow: Workflow) {
-    this.uid = workflow.uid;
-    this.createdAt = workflow.createdAt;
-    this.startedAt = workflow.createdAt;
-    this.finishedAt = workflow.finishedAt;
-    this.name = workflow.name;
-    this.phase = workflow.phase;
-  }
-
-  get active(): boolean {
-    const phase = this.phase;
-    if(!phase) {
-      return false;
+    constructor(workflow: Workflow) {
+        this.uid = workflow.uid;
+        this.createdAt = workflow.createdAt;
+        this.startedAt = workflow.createdAt;
+        this.finishedAt = workflow.finishedAt;
+        this.name = workflow.name;
+        this.phase = workflow.phase;
     }
 
-    return SimpleWorkflowDetail.activePhases[phase];
-  }
+    get active(): boolean {
+        const phase = this.phase;
+        if (!phase) {
+            return false;
+        }
 
-  get succeeded(): boolean {
-    const phase = this.phase;
-    if(!phase) {
-      return false;
+        return SimpleWorkflowDetail.activePhases[phase];
     }
 
-    return this.phase === 'Succeeded';
-  }
+    get succeeded(): boolean {
+        const phase = this.phase;
+        if (!phase) {
+            return false;
+        }
 
-  get terminated(): boolean {
-    const phase = this.phase;
-    if(!phase) {
-      return false;
+        return this.phase === 'Succeeded';
     }
 
-    return this.phase === 'Terminated';
-  }
+    get terminated(): boolean {
+        const phase = this.phase;
+        if (!phase) {
+            return false;
+        }
 
-  updateWorkflowManifest(manifest: string) {
-    this.jsonManifest = JSON.parse(manifest);
-    const status = this.jsonManifest.status;
-
-    this.startedAt = status.startedAt;
-    this.finishedAt = status.finishedAt;
-    this.phase = status.phase;
-  }
-
-
-  get workflowStatus(): WorkflowStatus|null {
-    if(!this.jsonManifest) {
-      return null;
+        return this.phase === 'Terminated';
     }
 
-    return this.jsonManifest.status;
-  }
+    updateWorkflowManifest(manifest: string) {
+        this.jsonManifest = JSON.parse(manifest);
+        const status = this.jsonManifest.status;
+
+        this.startedAt = status.startedAt;
+        this.finishedAt = status.finishedAt;
+        this.phase = status.phase;
+    }
+
+
+    get workflowStatus(): WorkflowStatus | null {
+        if (!this.jsonManifest) {
+            return null;
+        }
+
+        return this.jsonManifest.status;
+    }
 }
 
 @Injectable()
 export class WorkflowService {
-  watchWorkflow(namespace: string, name: string) {
-    const url =`${environment.baseWsUrl}/apis/v1beta1/${namespace}/workflow_executions/${name}/watch`;
+    watchWorkflow(namespace: string, name: string) {
+        const url = `${environment.baseWsUrl}/apis/v1beta1/${namespace}/workflow_executions/${name}/watch`;
 
-    return new WebSocket(url);
-  }
+        return new WebSocket(url);
+    }
 
-  watchLogs(namespace: string, workflowName: string, podId: string, containerName = 'main') {
-    const url =`${environment.baseWsUrl}/apis/v1beta1/${namespace}/workflow_executions/${workflowName}/pods/${podId}/containers/${containerName}/logs`;
+    watchLogs(namespace: string, workflowName: string, podId: string, containerName = 'main') {
+        const url = `${environment.baseWsUrl}/apis/v1beta1/${namespace}/workflow_executions/${workflowName}/pods/${podId}/containers/${containerName}/logs`;
 
-    return new WebSocket(url);
-  }
+        return new WebSocket(url);
+    }
 }
 
