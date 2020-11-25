7f09ff91840f4e17ae4f2d221736fbd7696b8d41
Andrey Melnikov <vafilor@gmail.com>
2020-11-25 10:56:52 -0800

    chore: formatted code

--- src/app/pipes/phase-translate/phase-translate.pipe.ts
+++ src/app/pipes/phase-translate/phase-translate.pipe.ts
@@ -2,15 +2,15 @@ import { Pipe, PipeTransform } from '@angular/core';
 import { WorkflowPhase } from '../../workflow/workflow.service';
 
 @Pipe({
-  name: 'phaseTranslate'
+    name: 'phaseTranslate'
 })
 export class PhaseTranslatePipe implements PipeTransform {
-  transform(phase: WorkflowPhase, ...args: any[]): any {
-    switch (phase) {
-      case 'Succeeded':
-        return 'Completed';
-    }
+    transform(phase: WorkflowPhase, ...args: any[]): any {
+        switch (phase) {
+            case 'Succeeded':
+                return 'Completed';
+        }
 
-    return phase;
-  }
+        return phase;
+    }
 }
