e05ebc9c3cbb8325928c02a7035c077770669fb8
Andrey Melnikov <vafilor@gmail.com>
2020-11-25 10:56:39 -0800

    fix: updated phase-translate to handle Omitted case.

--- src/app/pipes/phase-translate/phase-translate.pipe.ts
+++ src/app/pipes/phase-translate/phase-translate.pipe.ts
@@ -1,21 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { WorkflowPhase } from "../../workflow/workflow.service";
+import { WorkflowPhase } from '../../workflow/workflow.service';
 
 @Pipe({
   name: 'phaseTranslate'
 })
 export class PhaseTranslatePipe implements PipeTransform {
-  static statusPhraseMap = new Map<WorkflowPhase, string>([
-    ['Succeeded', 'Completed'],
-    ['Pending', 'Pending'],
-    ['Running', 'Running'],
-    ['Failed', 'Failed'],
-    ['Error', 'Error'],
-    ['Skipped', 'Skipped'],
-    ['Terminated', 'Terminated']
-  ]);
-
   transform(phase: WorkflowPhase, ...args: any[]): any {
-    return PhaseTranslatePipe.statusPhraseMap.get(phase);
+    switch (phase) {
+      case 'Succeeded':
+        return 'Completed';
+    }
+
+    return phase;
   }
 }
