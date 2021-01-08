98b47e324c16f244e6db2082f15e299cec74e38e
Andrey Melnikov <vafilor@gmail.com>
2021-01-08 11:22:37 -0800

    chore: code cleanup

--- src/app/cron-workflow/cron-workflow-edit-dialog/cron-workflow-edit-dialog.component.ts
+++ src/app/cron-workflow/cron-workflow-edit-dialog/cron-workflow-edit-dialog.component.ts
@@ -1,100 +1,97 @@
 import { Component, Inject, OnInit, ViewChild } from '@angular/core';
-import { FormComponent } from "../../fields/form/form.component";
-import { CronWorkflow, KeyValue, Parameter } from "../../../api";
-import { NamespaceTracker } from "../../namespace/namespace-tracker.service";
-import { Router } from "@angular/router";
-import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
-import { CronWorkflowFormatter } from "../models";
+import { FormComponent } from '../../fields/form/form.component';
+import { CronWorkflow, KeyValue, Parameter } from '../../../api';
+import { NamespaceTracker } from '../../namespace/namespace-tracker.service';
+import { Router } from '@angular/router';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { CronWorkflowFormatter } from '../models';
 import * as yaml from 'js-yaml';
 
 export interface CronWorkflowEditData {
-  cronWorkflow: CronWorkflow,
-  manifest: string,
+    cronWorkflow: CronWorkflow;
+    manifest: string;
 }
 
 @Component({
-  selector: 'app-cron-workflow-edit-dialog',
-  templateUrl: './cron-workflow-edit-dialog.component.html',
-  styleUrls: ['./cron-workflow-edit-dialog.component.scss']
+    selector: 'app-cron-workflow-edit-dialog',
+    templateUrl: './cron-workflow-edit-dialog.component.html',
+    styleUrls: ['./cron-workflow-edit-dialog.component.scss']
 })
 export class CronWorkflowEditDialogComponent implements OnInit {
-  @ViewChild(FormComponent, {static: false}) form: FormComponent;
+    @ViewChild(FormComponent, {static: false}) form: FormComponent;
 
-  parameters: Array<Parameter> = [];
-  labels = new Array<KeyValue>();
-  schedulingText = '';
+    parameters: Array<Parameter> = [];
+    labels = new Array<KeyValue>();
+    schedulingText = '';
 
-  constructor(
-      private namespaceTracker: NamespaceTracker,
-      private router: Router,
-      public dialogRef: MatDialogRef<CronWorkflowEditDialogComponent>,
-      @Inject(MAT_DIALOG_DATA) public data: CronWorkflowEditData) {
-    this.setManifest(data.cronWorkflow.manifest);
-    this.schedulingText = CronWorkflowFormatter.toYamlString(data.cronWorkflow, true);
-    if(data.cronWorkflow.labels) {
-      this.labels = data.cronWorkflow.labels;
-    }
-  }
-
-  ngOnInit() {
-  }
-
-  private setManifest(manifest: string) {
-    let parsedManifest = yaml.safeLoad(manifest);
-    let parameters = [];
-
-    if(parsedManifest.workflowSpec && parsedManifest.workflowSpec.arguments) {
-      if(!parsedManifest.workflowSpec.arguments.parameters) {
-        return [];
-      }
-
-      for (let dataParam of parsedManifest.workflowSpec.arguments.parameters) {
-        parameters.push(dataParam);
-      }
+    constructor(
+        private namespaceTracker: NamespaceTracker,
+        private router: Router,
+        public dialogRef: MatDialogRef<CronWorkflowEditDialogComponent>,
+        @Inject(MAT_DIALOG_DATA) public data: CronWorkflowEditData) {
+        this.setManifest(data.cronWorkflow.manifest);
+        this.schedulingText = CronWorkflowFormatter.toYamlString(data.cronWorkflow, true);
+        if (data.cronWorkflow.labels) {
+            this.labels = data.cronWorkflow.labels;
+        }
     }
 
-    this.parameters = parameters;
-  }
-
-  getData() {
-    let data = {
-      labels: this.labels,
-      workflowExecution: {
-        parameters: this.parameters
-      },
-      cron: CronWorkflowFormatter.fromYaml(this.schedulingText)
-    };
-
-    return data;
-  }
-
-  cancel() {
-    this.dialogRef.close();
-  }
-
-  save() {
-    if(!this.form.form.valid) {
-      this.form.form.markAllAsTouched();
-      return;
+    ngOnInit() {
     }
 
-    const data = this.getData();
+    private setManifest(manifest: string) {
+        const parsedManifest = yaml.safeLoad(manifest);
+        const parameters = [];
 
-    if(!data) {
-      return;
+        if (parsedManifest.workflowSpec && parsedManifest.workflowSpec.arguments) {
+            if (!parsedManifest.workflowSpec.arguments.parameters) {
+                return [];
+            }
+
+            for (const dataParam of parsedManifest.workflowSpec.arguments.parameters) {
+                parameters.push(dataParam);
+            }
+        }
+
+        this.parameters = parameters;
     }
 
-    this.dialogRef.close(data);
-  }
+    getData() {
+        const data = {
+            labels: this.labels,
+            workflowExecution: {
+                parameters: this.parameters
+            },
+            cron: CronWorkflowFormatter.fromYaml(this.schedulingText)
+        };
 
-  goToEnvironmentVariables() {
-    this.dialogRef.afterClosed().subscribe(res => {
-      this.router.navigate(['/', this.namespaceTracker.activeNamespace, 'secrets']);
-    });
+        return data;
+    }
 
-    this.dialogRef.close();
-  }
+    cancel() {
+        this.dialogRef.close();
+    }
 
-  ngOnDestroy(): void {
-  }
+    save() {
+        if (!this.form.form.valid) {
+            this.form.form.markAllAsTouched();
+            return;
+        }
+
+        const data = this.getData();
+
+        if (!data) {
+            return;
+        }
+
+        this.dialogRef.close(data);
+    }
+
+    goToEnvironmentVariables() {
+        this.dialogRef.afterClosed().subscribe(res => {
+            this.router.navigate(['/', this.namespaceTracker.activeNamespace, 'secrets']);
+        });
+
+        this.dialogRef.close();
+    }
 }
--- src/app/workflow-template/workflow-template-view/workflow-template-view.component.ts
+++ src/app/workflow-template/workflow-template-view/workflow-template-view.component.ts
@@ -65,7 +65,6 @@ export class WorkflowTemplateViewComponent implements OnInit {
 
         this.updateShowWorkflowExecutionCallToAction();
     }
-
     get hasWorkflowExecutions(): boolean {
         return this._hasWorkflowExecutions;
     }
