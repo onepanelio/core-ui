9d709687e3cb63705a08dcfccd5125f98e8b09ed
Andrey Melnikov <vafilor@gmail.com>
2021-03-12 10:35:14 -0800

    fix: remove environment variable length limit. Add more error handling for API. Renamed 'Secret' to 'Environment variable'.

--- src/app/secrets/create-secret/create-secret.component.spec.ts
+++ src/app/secrets/create-secret/create-secret.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { CreateSecretComponent } from './create-secret.component';
 
 describe('CreateSecretComponent', () => {
-  let component: CreateSecretComponent;
-  let fixture: ComponentFixture<CreateSecretComponent>;
+    let component: CreateSecretComponent;
+    let fixture: ComponentFixture<CreateSecretComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ CreateSecretComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [CreateSecretComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CreateSecretComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(CreateSecretComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
